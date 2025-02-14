73413b3c38a22d970121394522d025ae62f4bc46
Tim Numé <greyhatchan@proton.me>
2025-02-15 00:59:58 +0100

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -5,8 +5,8 @@ import config from '../../../config.json';
 
 // Help
 export const help = async (args: string[]): Promise<string> => {
-  const commands = Object.keys(bin).sort().join(', ');
-  var c = ', ';
+  const commands = Object.keys(bin).sort().join('');
+  var c = '';
   for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
     if (i % 7 === 0) {
       c += Object.keys(bin).sort()[i - 1] + '\n';
